a7b9be20f243a290db72078ac1d2cfedc842dae2
Ali Nasrabadi <nasrabadiam@gmail.com>
2021-01-21 18:12:52 +0330

    Add podcast detail fragment

--- app/src/main/java/com/nasabadiam/koopod/ui/main/PodcastDetailFragment.kt
+++ app/src/main/java/com/nasabadiam/koopod/ui/main/PodcastDetailFragment.kt
@@ -14,35 +14,29 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import com.nasabadiam.koopod.R
 
-class PodcastListFragment : Fragment() {
-
-    private val viewModel: PodcastListViewModel by viewModels()
+class PodcastDetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = ComposeView(requireContext()).apply {
         setContent {
-            PodcastListScreen(findNavController())
+            PodcastDetailScreen()
         }
     }
 }
 
 @Composable
-fun PodcastListScreen(navController: NavController) {
+fun PodcastDetailScreen() {
     Text("Hello World", Modifier.padding(16.dp))
     Button(
         onClick = {
-            navController.navigate(R.id.toPodCastDetail)
+
         }, colors = ButtonDefaults.buttonColors(
             backgroundColor = Color.Red
         )
     ) {
-        Text("To Podcast detail")
+        Text("Button")
     }
 }

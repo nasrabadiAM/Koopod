7716f2017182c746faa90056774a60856702cccb
Ali Nasrabadi <nasrabadiam@gmail.com>
2021-01-21 18:12:29 +0330

    Rename main activity xml file

--- app/src/main/java/com/nasabadiam/koopod/MainActivity.kt
+++ app/src/main/java/com/nasabadiam/koopod/MainActivity.kt
@@ -2,11 +2,16 @@ package com.nasabadiam.koopod
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import com.nasabadiam.koopod.databinding.ContentMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContent {
+            AndroidViewBinding(ContentMainBinding::inflate)
+        }
     }
 }

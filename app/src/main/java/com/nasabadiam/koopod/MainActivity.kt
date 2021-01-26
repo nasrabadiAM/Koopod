684e18a17c112afd42508aebc02cc989202fe8f8
Ali Nasrabadi <nasrabadiam@gmail.com>
2021-01-26 12:58:20 +0330

    Add setContentView to MainActivity

--- app/src/main/java/com/nasabadiam/koopod/MainActivity.kt
+++ app/src/main/java/com/nasabadiam/koopod/MainActivity.kt
@@ -2,16 +2,11 @@ package com.nasabadiam.koopod
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.viewinterop.AndroidViewBinding
-import com.nasabadiam.koopod.databinding.ContentMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AndroidViewBinding(ContentMainBinding::inflate)
-        }
+        setContentView(R.layout.content_main)
     }
 }

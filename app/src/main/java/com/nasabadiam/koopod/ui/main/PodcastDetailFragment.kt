4064054bdc6f6af78ea4e038670e09217a7fc019
Ali Nasrabadi <nasrabadiam@gmail.com>
2021-01-26 13:13:31 +0330

    Remove jetpack from podcast detail fragment

--- app/src/main/java/com/nasabadiam/koopod/ui/main/PodcastDetailFragment.kt
+++ app/src/main/java/com/nasabadiam/koopod/ui/main/PodcastDetailFragment.kt
@@ -4,39 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import com.nasabadiam.koopod.R
 
 class PodcastDetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = ComposeView(requireContext()).apply {
-        setContent {
-            PodcastDetailScreen()
-        }
-    }
-}
-
-@Composable
-fun PodcastDetailScreen() {
-    Text("Hello World", Modifier.padding(16.dp))
-    Button(
-        onClick = {
-
-        }, colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color.Red
-        )
-    ) {
-        Text("Button")
+    ): View {
+        return inflater.inflate(R.layout.fragment_podcast_list, container, false)
     }
 }

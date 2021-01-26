a6188a77e5cecc1ccf91a8a0fba21893b6743b01
Ali Nasrabadi <nasrabadiam@gmail.com>
2021-01-26 20:12:19 +0330

    Change navigation flow type to shared flow

--- app/src/main/java/com/nasabadiam/koopod/ui/podcastlist/PodcastListViewModel.kt
+++ app/src/main/java/com/nasabadiam/koopod/ui/podcastlist/PodcastListViewModel.kt
@@ -5,7 +5,9 @@ import androidx.navigation.NavDirections
 import com.nasabadiam.koopod.ResourceState
 import com.nasabadiam.koopod.utils.BaseViewModel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
@@ -17,8 +19,8 @@ class PodcastListViewModel : BaseViewModel() {
     private val _data = MutableStateFlow<List<PodcastItem>>(listOf())
     val data: StateFlow<List<PodcastItem>> = _data
 
-    private val _navigation = MutableStateFlow<NavDirections?>(null)
-    val navigation: StateFlow<NavDirections?> = _navigation
+    private val _navigation = MutableSharedFlow<NavDirections?>()
+    val navigation: SharedFlow<NavDirections?> = _navigation
 
     private val _message = MutableStateFlow("")
     val message: StateFlow<String> = _message
@@ -29,7 +31,6 @@ class PodcastListViewModel : BaseViewModel() {
             delay(1000)
             _state.emit(ResourceState.SuccessEmpty)
             _message.emit("No Data Available to show!")
-
 
             ////
             delay(1000)
@@ -44,7 +45,12 @@ class PodcastListViewModel : BaseViewModel() {
                 "https://ssl-static.libsyn.com/p/assets/3/2/4/8/32482d0e2dd140d2/channelb-logo-red.png"
             _data.emit(
                 listOf(
-                    PodcastItem("id1", "پادکست علی بندری", imageUrl, "در هر اپیزود پادکست فارسی چنل\u200Cبی گزارش یک ماجرای واقعی را به نقل از یک رسانه\u200Cی معتبر انگلیسی\u200Cزبان برای شما تعریف می\u200Cکنیم."),
+                    PodcastItem(
+                        "id1",
+                        "پادکست علی بندری",
+                        imageUrl,
+                        "در هر اپیزود پادکست فارسی چنل\u200Cبی گزارش یک ماجرای واقعی را به نقل از یک رسانه\u200Cی معتبر انگلیسی\u200Cزبان برای شما تعریف می\u200Cکنیم."
+                    ),
                     PodcastItem("id2", "پادکست ده صبح", imageUrl, "desc"),
                     PodcastItem("id3", " کتابخوان", imageUrl, "desc"),
                     PodcastItem("id4", "سلام به همه", imageUrl, "desc"),

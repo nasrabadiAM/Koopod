ad1a0f7647eab5263e7cae687cfb26bd7df7efef
Ali Nasrabadi <nasrabadiam@gmail.com>
2021-01-26 20:10:34 +0330

    Generalize fragment extension function to use flow instead of state flow

--- app/src/main/java/com/nasabadiam/koopod/utils/FragmentExt.kt
+++ app/src/main/java/com/nasabadiam/koopod/utils/FragmentExt.kt
@@ -2,11 +2,11 @@ package com.nasabadiam.koopod.utils
 
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
-fun <T> Fragment.collectAsState(flow: StateFlow<T>, func: (t: T) -> Unit) {
+fun <T> Fragment.collectAsState(flow: Flow<T>, func: (t: T) -> Unit) {
     viewLifecycleOwner.lifecycleScope.launch {
         flow.collect {
             func.invoke(it)
